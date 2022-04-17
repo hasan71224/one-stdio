2c8245a7d2a93dc3ab5d6ed6299f0616d00723fd
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-04-17 16:44:28 +0600

    functional login option

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 
 const Login = () => {
     const navigate = useNavigate()
     const location = useLocation()
-    
+    const emailRef = useRef(' ')
+    const passwordRef = useRef(' ')
+    let from = location.state?.from?.pathname || "/";
 
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (user) {
+        navigate(from, { replace: true })
+    }
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password)
+
+    }
     let errorElement;
+    if (error) {
+        errorElement =
+            <div>
+                <p className='text-danger'>Error: {error?.message}</p>
+            </div>
 
-    // if (error) {
-    //     errorElement =
-    //         <div>
-    //             <p className='text-danger'>Error: {error?.message}</p>
-    //         </div>
-
-    // }
+    }
 
     const navigateRegister = event => {
         navigate('/register')
@@ -26,10 +46,10 @@ const Login = () => {
         <div>
             <div className='w-25 mx-auto border border-1 rounded-3 p-3 mt-5 shadow p-3 mb-5 bg-body rounded'>
                 <h2 className='text-center mb-3 text-primary'>Please Login</h2>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" required />
+                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -37,11 +57,9 @@ const Login = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" required />
+                        <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
+
                     <Button className='w-50 mx-auto d-block mt-3' variant="primary" type="submit">
                         LogIn
                     </Button>
