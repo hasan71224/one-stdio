22489f5fe3f1905ed3373d5acaa7f5bb163df92e
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-04-17 22:53:32 +0600

    complite about and blog page and checkout page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,7 +2,8 @@ import React from 'react';
 import about from '../../images/about.jpg'
 const About = () => {
     return (
-        <div class="card mb-3 w-100" >
+        <div>
+            <div class="card mb-3 w-100" >
             <div class="row g-0">
                 <div class="col-md-4">
                     <img src={about} className="img-fluid rounded-start" alt="..." />
@@ -10,11 +11,12 @@ const About = () => {
                 <div class="col-md-8">
                     <div class="card-body">
                         <h2 class="card-title">Md. Mahmudul Hasan</h2>
-                        <p class="card-text"> <strong>Goal: </strong> Assalamualaikum- I'm Mahmudul Hasan, This year i'm complete my graduation. now i'm trying to learn Front End Development. So, This year My Goal is SuccessFully Complete Programming hero course and find a job as a front end developer. and Learn some new topic Angular, Mongo DB, Note JS and More. I need to see me as Full Stack Developer. Robotics is my favorite subject, i need success in this site. in this year i'm trying to complete one certification in Robotic Process Automation.  </p>
+                        <p class="card-text"> <strong>Goal: </strong> Assalamualaikum- I'm Mahmudul Hasan, This year i'm complete my graduation. now i'm trying to learn Front End Development. So, This year My Goal is SuccessFully Complete Programming hero course and find a job as a front end developer. and Learn some new topic Angular, Mongo DB, Note JS and More. I need to see me as Full Stack Developer. Robotics is my favorite subject, i need success in this site. in this year i'm trying to complete one certification in Robotic Process Automation. UI Path RPA Associate and UI Path RPA Advance is my target certification. </p>
                         
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Alert, Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const CheckOut = () => {
+    const navigation=useNavigate()
+    const handleSubmit = event =>{
+       alert('Thanks For Submit')
+       navigation('/')
+    }
     return (
-        <div>
-            <h1>Please Checkout your Booking</h1>
+        <div className=''>
+            <div className='w-25 mx-auto border border-1 rounded-3 p-3 mt-5 shadow p-3 mb-5 bg-body rounded'>
+                <h2 className='text-center mb-3 text-primary'>CheckOut Form</h2>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Full Name</Form.Label>
+                        <Form.Control name='name' type="text" placeholder="Enter your name" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control name='email' type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Phone Number</Form.Label>
+                        <Form.Control name='phone' type="text" placeholder="phone number"/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control name='Address' type="text" placeholder="Address"/>
+                    </Form.Group>
+                    
+                    <Button className='w-50 mx-auto d-block mt-3' variant="primary" type="submit" onClick={handleSubmit}>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 };
